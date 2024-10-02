84a0be9721955ffcf1d11272113e5c49cde410ae
Pethai Napanaruemit <53882701+pethai2004@users.noreply.github.com>
2024-10-02 13:55:52 +0700

    v1

--- src/data_utility.py
+++ src/data_utility.py
@@ -25,74 +25,6 @@ class AudioTrainingConfig:
     per_device_batch_size: int = 32
     pin_memory: bool = True
     persistent_workers: bool= True
-    
-def load_validate_audio(audiopath, sampling_rate):
-    audio, lsr = torchaudio.load(audiopath)
-
-    if audio.size(0) != 1:
-        audio = torch.mean(audio, dim=0, keepdim=True)
-
-    if lsr != sampling_rate:
-        audio = torchaudio.functional.resample(audio, lsr, sampling_rate)
-    if torch.any(audio > 10) or not torch.any(audio < 0):
-        raise ValueError(f"Audio not normalized: {audio.min()}, {audio.max()}")
-
-    audio.clip_(-1, 1)
-    if audio.shape[-1] < (0.5 * sampling_rate):
-        raise ValueError(f"Audio too short: {audio.shape[-1]}")
-    
-    return audio
-
-class AudioDataset(Dataset):
-    
-    def __init__(self, samples, max_wav_lengt=255955, sampling_rate=22050):
-        torch.set_num_threads(1)
-        self.samples = []
-        for s in samples:
-            try:
-                load_validate_audio(s['audio_file'], sampling_rate)
-            except Exception as e:
-                logging.warning(f"Error loading {s['audio_file']}: {e}")
-                continue
-            self.samples.append(s)
-        logging.info(f"Loaded and validated {len(self.samples)} samples out of {len(samples)}")
-            
-        self.max_wav_length = max_wav_lengt # 255955 (approx 11.6 secs) which is supported by model (see in model.arg)
-        self.sampling_rate = sampling_rate
-        self._failed_idx = set()
-        
-    def __len__(self):  
-        return len(self.samples)
-    
-    def __getitem__(self, idx):
-        # Always assume that the audio data is valid, otherwise ignore
-        
-        if idx in self._failed_idx:
-            return self.__getitem__(idx + 1)
-        try:
-            wav = load_validate_audio(self.samples[idx]['audio_file'], self.sampling_rate)
-        except Exception as e:
-            logging.error(f"Error loading {self.samples[idx]['audio_file']}: {e}")
-            if not idx in self._failed_idx:
-                self._failed_idx.add(idx)
-            #try calling the next index
-            return self.__getitem__(idx + 1)
-        
-        wav_length = torch.tensor(wav.shape[-1], dtype=torch.long)
-        audio_dict = {"wav": wav, 'wav_length': wav_length, 'audio_path': self.samples[idx]['audio_file']}
-        return audio_dict
-
-def audio_collate_fn(batch):
-    '''Collate function for DVAE dataset'''
-    
-    batch = {k: [dic[k] for dic in batch] for k in batch[0]}
-    batch["wav_length"] = torch.stack(batch["wav_length"])
-    # note batch['wav'] has shape [1, seq_length]
-    wav_padded = pad_sequence([wav[0] for wav in batch["wav"]], batch_first=True)
-    wav_padded = wav_padded.unsqueeze(1)    # Add a channel dimension
-    batch["wav"] = wav_padded
-    
-    return batch
 
 def create_audio_dataloader(
     meta_data,
@@ -157,3 +89,8 @@ def _put(tensor, device, dtype=None, non_blocking=True):
         )
     else:
         return tensor
+
+
+def combine_dataset(dataset_list):
+    
+    return None 
